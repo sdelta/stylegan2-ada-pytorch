165b6040c64acf562b04ad4539c25d108888c09d
sdelta <rma-karpichev@rambler.ru>
2022-12-08 07:43:15 +0700

    fix gradfix: _jit_get_operation is now return tuple

--- torch_utils/ops/grid_sample_gradfix.py
+++ torch_utils/ops/grid_sample_gradfix.py
@@ -57,7 +57,7 @@ class _GridSample2dForward(torch.autograd.Function):
 class _GridSample2dBackward(torch.autograd.Function):
     @staticmethod
     def forward(ctx, grad_output, input, grid):
-        op = torch._C._jit_get_operation('aten::grid_sampler_2d_backward')
+        op, _ = torch._C._jit_get_operation('aten::grid_sampler_2d_backward')
         if _use_pytorch_1_11_api:
             output_mask = (ctx.needs_input_grad[1], ctx.needs_input_grad[2])
             grad_input, grad_grid = op(grad_output, input, grid, 0, 0, False, output_mask)
