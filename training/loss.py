fix wrong sign in clip loss

training/loss.py
@@ -161,7 +161,7 @@ class StyleGAN2Loss(Loss):
         if do_clip:
             with torch.autograd.profiler.record_function('clip_forward'):
                 gen_img, _gen_ws = self.run_G(gen_z, gen_c, sync=False)
-                gen_clip = self.clip_subloss.get_similarities(gen_img)
+                gen_clip = -self.clip_subloss.get_similarities(gen_img)
                 training_stats.report('Loss/clip/prob', gen_clip)
             with torch.autograd.profiler.record_function('clip_backward'):
                 gen_clip.mean().mul(gain).backward()