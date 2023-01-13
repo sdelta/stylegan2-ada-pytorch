60ca3dd7a19e4c3b974b0cc03bcae2760948e496
sdelta <rma-karpichev@outlook.com>
2023-01-13 10:39:13 +0000

    fix simple mistakes in latent optimization
    it still doesn't work, but at least changes the picture now

--- optimize_latents.py
+++ optimize_latents.py
@@ -10,6 +10,7 @@
 
 import copy
 import os
+from typing import Optional
 from time import perf_counter
 
 import click
@@ -21,16 +22,18 @@ import torch.nn.functional as F
 
 import dnnlib
 import legacy
+from training.loss import CLIPSubloss
+
 
 def transfer(
     G,
     CLIP_loss,
     source: torch.Tensor, # [C,H,W] and dynamic range [0,255], W & H must match G output resolution
+    device: torch.device,
     num_steps                  = 200,
     alpha                      = 0.005,
     lr                         = 0.05,
-    verbose                    = False,
-    device: torch.device
+    verbose                    = True,
 ):
     def logprint(*args):
         if verbose:
@@ -43,18 +46,20 @@ def transfer(
 
     optimizer = torch.optim.Adam([result], betas=(0.9, 0.999), lr=lr)
 
-    for step in range(num_steps)
+    for step in range(num_steps):
         optimizer.zero_grad(set_to_none=True)
         gen_img = G.synthesis(result)
-        gen_clip = -self.clip_subloss.get_similarities(gen_img).mean()
-        loss = gen_clip + alpha * torch.linalg.norm(result - source)
+        phrase_loss = -CLIP_loss.get_similarities(gen_img).mean()
+        reg = alpha * torch.linalg.norm(result - source)
+        loss = phrase_loss + reg
+        logprint(f'step {step}  clip_loss {phrase_loss:<4.2f}  reg_loss {reg:<4.2f}')
         loss.backward()
         optimizer.step()
-    return result
+    return result.detach()
 
 
-def generate_and_save(w, fname):
-    img = G.synthesis(w.unsqueeze(0))
+def generate_and_save(G, w, fname):
+    img = G.synthesis(w)
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
     img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(fname)
 
@@ -65,7 +70,8 @@ def generate_and_save(w, fname):
 @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
 @click.option('--source_prefix', 'source_prefix', help='where to save unmodified image', metavar='FILE')
 @click.option('--projected-w', help='file with projection of the source image', type=str, metavar='FILE', required=True)
-@click.option('--num-steps', help='Number of optimization steps', type=int, default=1000, show_default=True)
+@click.option('--phrase', help='to which phrase resulting picture should conform', type=str, metavar='FILE', required=True)
+@click.option('--num-steps', help='Number of optimization steps', type=int, default=200, show_default=True)
 @click.option('--alpha', help='L2 reg coefficient', type=float, default=0.005, show_default=True)
 @click.option('--lr', help='learning rate', type=float, default=0.05, show_default=True)
 @click.option('--outdir', help='Where to save the output images', required=True, metavar='DIR')
@@ -73,6 +79,7 @@ def run_projection(
     network_pkl: str,
     source_prefix: Optional[str],
     projected_w: str,
+    phrase: str,
     num_steps: int,
     alpha: float,
     lr: float,
@@ -87,27 +94,28 @@ def run_projection(
         --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl
     """
     device = torch.device('cuda')
-    clip_loss = ClipSubloss(device, phrase)
+    clip_loss = CLIPSubloss(device, phrase)
 
     # Load networks.
     print('Loading networks from "%s"...' % network_pkl)
     with dnnlib.util.open_url(network_pkl) as fp:
         G = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device) # type: ignore
 
+    os.makedirs(outdir, exist_ok=True)
+        
     ws = np.load(projected_w)['w']
     ws = torch.tensor(ws, device=device) # pylint: disable=not-callable
     assert ws.shape[1:] == (G.num_ws, G.w_dim)
     for idx, w in enumerate(ws):
         print('Modifying image #{}'.format(idx))
         new_w = transfer(
-            G, clip_loss, w.unsqueeze(0),
-            num_steps, alpha, lr,
-            device
+            G, clip_loss, w.unsqueeze(0), device,
+            num_steps, alpha, lr
         )
         np.savez(f'{outdir}/projected_w_{idx:02d}.npz', w=new_w.cpu().numpy())
-        generate_img(new_w, f'{outdir}/result_{idx:02d}.png')
+        generate_and_save(G, new_w, f'{outdir}/result_{idx:02d}.png')
         if source_prefix is not None:
-            generate_img(w, f'{outdir}/{source_prefix}_{idx:02d}.png')
+            generate_and_save(G, w.unsqueeze(0), f'{outdir}/{source_prefix}_{idx:02d}.png')
 
 
 #----------------------------------------------------------------------------
